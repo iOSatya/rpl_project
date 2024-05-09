66aeb9ab3e7c5b050fc5c673ec656584f7af08f4
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-05-09 15:56:33 +0700

    Update database.config.php

--- config/database.config.php
+++ config/database.config.php
@@ -1,1 +1,16 @@
+<?php
 
+
+$dbId = "mysql:host=localhost; dbname=rpl_project"; // host buat url databasenya, dbname buat nama databasenya
+$dbUsername = "root"; // nama user yg punya database
+$dbPassword = ""; // password user yg punya database
+
+try {
+    // koneksi ke database
+    $pdo = new PDO($dbId, $dbUsername, $dbPassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // kalau koneksi gagal
+    echo "Error: " . $e->getMessage();
+    die();
+}
