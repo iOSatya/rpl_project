9efa2068f9b3b2998466d3b9918da76110400907
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-05-09 16:29:07 +0700

    Update login.php

--- view/login.php
+++ view/login.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div class="d-flex justify-content-center">
+            <form class="d-flex flex-column justify-content-center" style="height: 100vh;">
+                <h2>Login</h2>
+                <input type="email" placeholder="E-Mail" class="form-control mb-2">
+                <input type="password" placeholder="Password" class="form-control mb-2">
+                <a href="" class="align-self-end mb-2">I don't have an account</a>
+                <button type="submit" class="btn btn-primary align-self-start">Login</button>
+            </form>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
