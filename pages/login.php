cca51548d39166124a97b7f8faff1ba0aa9994c8
iOSatya <atthoriqsatya@outlook.com>
2024-05-10 15:24:04 +0700

    Update

--- pages/login.php
+++ pages/login.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    </head>
-    <body>
         <div class="d-flex justify-content-center align-items-center" style="height: 100vh; background-image: url('images/Prosperous_fantasy_kingdom_landscape.png'); background-size: cover; background-position: 0px -100px;">
             <form class="d-flex flex-column bg-white bg-opacity-10 p-4">
                 <h2>Login</h2>
@@ -18,6 +7,3 @@
                 <button type="submit" class="btn btn-primary align-self-start">Login</button>
             </form>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-</html>
