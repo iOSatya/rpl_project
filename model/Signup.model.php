5e7ea9f810d006032d76f44ec23de3857b374a48
iOSatya <atthoriqsatya@outlook.com>
2024-05-10 14:08:11 +0700

    Update Signup.model.php

--- model/Signup.model.php
+++ model/Signup.model.php
@@ -10,4 +10,10 @@ class Signup extends Database {
         $this->pwd = $pwd;
         $this->playerStatus = $playerStatus;
     }
+
+    private function insertUser() {
+        $query = "insert into players (email, pwd, player_status) values (?, ?, ?);";
+        $stmt = parent::dbConnect()->prepare($query);
+        $stmt->execute([$this->email, $this->pwd, $this->playerStatus]);
+    }
 }
