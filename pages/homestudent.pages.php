5e66c1e0e57788f4dd66fca452264db89b65932c
iOSatya <atthoriqsatya@outlook.com>
2024-05-16 18:06:54 +0700

    Update homestudent.pages.php

--- pages/homestudent.pages.php
+++ pages/homestudent.pages.php
@@ -16,7 +16,7 @@
     </a>
     <br class="m-3">
     <a href="#" class="card link-underline link-underline-opacity-0" style="width: 18rem; height: 75vh;">
-        <img src="../pages/images/Prosperous_fantasy_kingdom_landscape.png" class="card-img-top" alt="...">
+        <img src="../pages/images/Old_wooden_wardrobe.png" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Customize Your Character</h5>
             <p class="card-text">Reimagine your digital avatar by tweaking stats, upgrading traits, and unlocking new capabilities. Design the ideal persona to embark on epic learning quests!</p>
