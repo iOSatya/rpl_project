544cc59ed14500a073d92cdd0408d8cdb7b9b784
iOSatya <atthoriqsatya@outlook.com>
2024-05-16 17:56:30 +0700

    Update homestudent.pages.php

--- pages/homestudent.pages.php
+++ pages/homestudent.pages.php
@@ -1,17 +1,17 @@
 <div class="d-flex justify-content-center align-items-center bg-dark" style="height: 100vh;">
     <a href="#" class="card link-underline link-underline-opacity-0" style="width: 18rem; height: 75vh;">
-        <img src="../pages/images/Prosperous_fantasy_kingdom_landscape.png" class="card-img-top" alt="...">
+        <img src="../pages/images/Prosperous_fantasy_kingdom_attacked_by_dark_monster.png" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Craft Your Epic Quest</h5>
-            <p class="card-text">Forge mind-bending inquiries to challenge students on an unforgettable learning adventure. Each question you craft is another twist in their epic educational quest!</p>
+            <h5 class="card-title">Begin Your Questing Trials</h5>
+            <p class="card-text">Steel your mind for the intellectual gauntlet ahead. Embark on questing trials crafted to push your knowledge and grit to the limits. Answer the call to adventure!</p>
         </div>
     </a>
     <br class="m-3">
     <a href="#" class="card link-underline link-underline-opacity-0" style="width: 18rem; height: 75vh;">
         <img src="../pages/images/Prosperous_fantasy_kingdom_landscape.png" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Explore the Realm of Challenges</h5>
-            <p class="card-text">Venture forth into the territory of mind-bending queries you've crafted to test your students' courage and cunning. Survey the treacherous terrain of your educational adventures!</p>
+            <h5 class="card-title">The Illumination Leaderboards</h5>
+            <p class="card-text">Behold the luminous ranks where intellectual giants dwell, basking in transcendent knowledge. Ascend through dedicated striving to join their refulgent realm!</p>
         </div>
     </a>
     <br class="m-3">
