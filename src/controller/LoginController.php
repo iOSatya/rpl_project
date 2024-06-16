63c3369db426707b742fc93e09a2336619266082
iOSatya <atthoriqsatya@outlook.com>
2024-06-16 14:06:18 +0700

    Update

--- src/controller/LoginController.php
+++ src/controller/LoginController.php
@@ -9,23 +9,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   try {
     $username = $_POST["username"];
     $userPassword = $_POST["userPassword"];
-  
+
     $model = new LoginModel();
   
     if (!$username || !$userPassword) {
+
       $_SESSION["errorMessage"] = "Field Must not Empty!";
       header("Location: ../view/LoginView.php");
       die();
+
     } else {
+
       if ($model->authenticateUser($username, $userPassword)) {
         $_SESSION["userData"] = $model->getUserData($username);
-        print_r($_SESSION["userData"]["userPassword"]);
+        print_r($_SESSION["userData"]["userFullName"]);
       } else {
         $_SESSION["errorMessage"] = "Wrong Username/Password!";
         header("Location: ../view/LoginView.php");
         die();
       }
+      
     }
+    
   } catch (PDOException $e) {
     echo $e->getMessage();
     die();
---
+++ src/view/HomeView.php
@@ -0,0 +1,12 @@
+<?php require_once "../config/config.php"; ?>
+<?php require_once "./HeaderView.php"; ?>
+  
+<?php if ($_SESSION["userData"]["status"] === "student") { ?>
+
+  <div>
+
+  </div>
+
+<? } ?>
+
+<?php require_once "./FooterView.php"; ?>
