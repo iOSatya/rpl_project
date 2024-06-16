1eabec5d0819acc1d58b024b148423d39c462219
iOSatya <atthoriqsatya@outlook.com>
2024-06-16 10:29:12 +0700

    Update

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-header("Location : src/view/LoginView.php");
+header("Location: ./src/view/LoginView.php");
--- src/view/LoginView.php
+++ src/view/LoginView.php
@@ -1,2 +1,5 @@
 <?php require_once "./HeaderView.php"; ?>
-  <div>Test</div>
+  
+  
+
+<?php require_once "./FooterView.php"; ?>
