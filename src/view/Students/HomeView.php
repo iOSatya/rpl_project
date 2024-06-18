ff07036c62355b622da20ab098011afa01ba8ba4
iOSatya <atthoriqsatya@outlook.com>
2024-06-18 13:07:41 +0700

    Update

--- src/view/Students/HomeView.php
+++ src/view/Students/HomeView.php
@@ -6,7 +6,7 @@
   <div>
     <a class="btn btn-danger" href="./../../controller/LogoutController.php">Logout</a>
     <a class="btn btn-primary" href="./MapView.php">Start Game</a>
-    <a class="btn btn-primary">Leaderboard</a>
+    <a class="btn btn-primary" href="./LeaderboardView.php">Leaderboard</a>
   </div>
 
 <?php
